2e92ab5cf1cfc7614365bf2d7220bb92510d8e0c
iwasjs <bshqwe7@gmail.com>
2024-07-15 11:50:03 +0300

    Update config.js
    S

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  token:
+  token:1241470294703472734
     "",
-  channel: "1187655235229995058",
+  channel: "1261766912124260413",
   data: "data.json",
   cf: {
     enable: true,
